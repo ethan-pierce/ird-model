b4c738c84b4ce4417a16818a1326436ec85e2ae6
ethan-pierce <ethan.pierce@colorado.edu>
2020-06-17 14:39:10 -0600

    Fix numerical errors in effective viscosity code

--- src/GlacierFDM.jl
+++ src/GlacierFDM.jl
@@ -1,12 +1,24 @@
 module GlacierFDM
 
+const zero = 1e-30
+const rho = 917 # kg m^-3
+const g = 9.81 # m s^-2
+const phi_CISM = 9.8e-8 # K Pa^-1
+const phi_CP = 7e-8 # K Pa^-1
+const A_0 = 2.4e-24 # s Pa^-n 
+const n = 3 
+const Q_plus = -1.15e5 # J mol^-1
+const Q_minus = -6e4 # J mol^-1
+const R = 8.3145 # J mol^-1 K^-1
+const transition_temperature = 263 # K
+
 mutable struct Grid
     shape::Tuple{UInt32, UInt32}
     h::Tuple{Float32, Float32}
     dx::Float32 
     dy::Float32
-    data::Array{Float64, 2}
-    Grid(shape, h) = new(shape, h, h[1], h[2], Array{Float64, 2}(undef, shape[2], shape[1]))
+    data::Array{BigFloat, 2}
+    Grid(shape, h) = new(shape, h, h[1], h[2], Array{BigFloat, 2}(undef, shape[2], shape[1]))
     Grid(shape, h, data) = new(shape, h, h[1], h[2], data)
 end
 
@@ -28,11 +40,20 @@ function set!(grid::Grid, fn::Function)
     return grid
 end
 
+function fill_zeros!(grid::Grid)
+    for i = 1:length(grid.data)
+        if grid.data[i] == 0
+            grid.data[i] = zero
+        end
+    end
+    return grid
+end
+
 function grad_x(grid::Grid)::Grid
     output = deepcopy(grid)
-    for xi = 2:output.shape[1] - 1
-        for xj = 1:output.shape[2]
-            output[xi, xj] = (1 / (2 * output.dx)) * (output[xi - 1, xj] - output[xi + 1, xj])
+    for xi = 2:grid.shape[1] - 1
+        for xj = 1:grid.shape[2]
+            output[xi, xj] = (1 / (2 * grid.dx)) * (grid[xi - 1, xj] - grid[xi + 1, xj])
         end
     end
     return output
@@ -40,9 +61,9 @@ end
 
 function grad_y(grid::Grid)::Grid
     output = deepcopy(grid)
-    for xi = 1:output.shape[1]
-        for xj = 2:output.shape[2] - 1
-            output[xi, xj] = (1 / (2 * output.dy)) * (output[xi, xj - 1] - output[xi, xj + 1])
+    for xi = 1:grid.shape[1]
+        for xj = 2:grid.shape[2] - 1
+            output[xi, xj] = (1 / (2 * grid.dy)) * (grid[xi, xj - 1] - grid[xi, xj + 1])
         end
     end
     return output
@@ -56,28 +77,50 @@ function calculate_strain(u::Grid, v::Grid)::Grid
     return epsilon
 end
 
-function B(temperature::Grid, H::Grid)::Grid
-    const rho = 917 # kg m^-3
-    const g = 9.81 # m s^-2
-    const phi = 9.8e-8 # K Pa^-1
-    const A_0 = 2.4e-24 # s Pa^-n 
-    const n = 3 
-    const Q = 115e3 # J mol^-1
-    const R = 8.3145 # J mol^-1 K^-1
-    T_adj = temperature.data .+ (rho * g * phi) .* H.data
-    A_array = A_0 .* exp(-Q ./ (R .* T_adj))
-    B_array = A_array.^(-1 / n)
+function set_creep_activation(T_effective::Array, T_transition::Array)::Array
+    if length(T_effective) != length(T_transition) && 
+        throw(DimensionMismatch("temperature arrays are not the same length"))
+    end
+    
+    Q_array = deepcopy(T_effective)
+
+    for i = 1:length(Q_array)
+        if T_effective[i] > T_transition[i]
+            Q_array[i] = Q_plus
+        else
+            Q_array[i] = Q_minus
+        end
+    end
+
+    return Q_array
+end
+
+function B(temperature::Grid, H::Grid)::Grid 
+    T_effective = temperature.data .+ (rho * g * phi_CP) .* H.data
+    T_transition = H.data .* (rho * g * phi_CP) .+ transition_temperature
+
+    Q_array = set_creep_activation(T_effective, T_transition)
+    A_array = A_0 .* exp.(Q_array ./ (R .* T_effective))
+
+    B_array = A_array .^ (-1 / n)
     B = deepcopy(temperature)
     B.data = B_array
     return B
 end
 
 function calculate_eta(strain::Grid, temperature::Grid, H::Grid)::Grid
-    const n = 3
-    B = B(temperature, H)
+    B_array = B(temperature, H)
     strain_norm = (1 / 2) .* strain.data .* strain.data
-    eta = deepcopy(temperature)
-    eta.data = (1 / 2) .* B.data .* (strain_norm .^ ((1 - n) / n))
+
+    for i = 1:length(strain_norm)
+        if strain_norm[i] == 0
+            strain_norm[i] = zero
+        end
+    end
+
+    eta = deepcopy(H)
+    eta.data = (1 / 2) .* B_array.data .* (strain_norm .^ ((1 - n) / n))
+    return eta
 end
 
 end
--- test/test_glacier_fdm.jl
+++ test/test_glacier_fdm.jl
@@ -1,7 +1,8 @@
 using Test
 
 include("../src/GlacierFDM.jl")
-using .GlacierFDM: Grid, set!, grad_x, grad_y
+using .GlacierFDM: Grid, set!, fill_zeros!, grad_x, grad_y,
+    calculate_strain, B, calculate_eta
 
 @testset "Constructors" begin
     g = Grid((7, 5), (1, 1))
@@ -19,11 +20,39 @@ using .GlacierFDM: Grid, set!, grad_x, grad_y
     set!(g, (x, y) -> x^2 + y)
     @test g.data[1, 3] == 10
     @test g[3, 1] == 10
-    
+end
+
+@testset "Gradients" begin
+    g = Grid((7, 5), (1, 1))
+    set!(g, (x, y) -> x^2 + y)
+
     xdiff = grad_x(g)
     @test xdiff[2, 1] == -4.0
 
     ydiff = grad_y(g)
     @test ydiff[1, 2] == -1
+end
 
+@testset "Effective viscosity" begin
+    u = Grid((4, 4), (1, 1))
+    set!(u, (x, y) -> 3*(x + y))
+
+    v = Grid((4,4), (1, 1))
+    set!(v, (x, y) -> 1 * (x + y))
+    
+    epsilon = calculate_strain(u, v)
+    @test epsilon[2, 2] == -2.0
+    @test epsilon[2, 2] == epsilon[2, 3]
+    @test epsilon[2, 2] == epsilon[3, 2]
+    fill_zeros!(epsilon)
+    epsilon
+
+    T = Grid((4, 4), (1, 1))
+    set!(T, (x, y) -> 265 + y + x)
+
+    H = Grid((4, 4), (1, 1))
+    set!(H, (x, y) -> 1000 + 2 * x)
+
+    Btest = B(T, H)
+    eta = calculate_eta(epsilon, T, H)
 end
